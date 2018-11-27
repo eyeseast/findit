633c3712d3e57f379b3a0fa6b5adad9a17e902cf
Chris Amico <eyeseast@gmail.com>
2018-11-26 22:08:29 -0500

    migrate to latest sapper

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 import sirv from 'sirv';
 import polka from 'polka';
-import sapper from 'sapper';
 import compression from 'compression';
-import { manifest } from './manifest/server.js';
+import * as sapper from '../__sapper__/server.js';
+
+const { PORT=3000, NODE_ENV='development' } = process.env;
+const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('data'), // where geographies live
-		sirv('assets'),
-		sapper({ manifest })
+		sirv('static'),
+		sapper.middleware(),
 	)
 	.listen(process.env.PORT)
 	.catch(err => {
